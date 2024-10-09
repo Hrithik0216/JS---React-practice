5aead045096d664fb3adca35a6f1bc971d90a607
hrithik+1-at-970287672632 <hrithik@salesgear.io>
2024-10-09 11:03:48 +0530

    Added blur effect

--- Marketing website 2/mb-2/src/component/Flowchart.js
+++ Marketing website 2/mb-2/src/component/Flowchart.js
@@ -6,19 +6,18 @@ export const Flowchart = () => {
       <ul>
         <li>
           <div className="li-container mailSent">
-          <div className='mailSent-div'>
-            <img src={emailSentIcon} alt="Email Sent Icon" className="icon" />
-            <a className="mailSentAnchor" href="#">
-              Email Sent
-            </a>
+            <div className="mailSent-div">
+              <img src={emailSentIcon} alt="Email Sent Icon" className="icon" />
+              <a className="mailSentAnchor" href="#">
+                Email Sent
+              </a>
             </div>
-
           </div>
 
           <ul>
             <li>
               <div className="li-container">
-                <div className='inner-li-container'>
+                <div className="inner-li-container">
                   <img
                     src={emailSentIcon}
                     alt="Email Sent Icon"
@@ -29,37 +28,44 @@ export const Flowchart = () => {
                   </div>
                 </div>
               </div>
+
               <ul>
                 <li>
-                  <div className="li-container">
-                  <div className='inner-li-container'>
-                    <img
-                      src={emailSentIcon}
-                      alt="Email Sent Icon"
-                      className="icon"
-                    />
-                    <a href="#">Pricing page visited</a>
+                  <div className="li-container first-child-extended"> {/* For extending line */}
+                    <div className="inner-li-container">
+                      <img
+                        src={emailSentIcon}
+                        alt="Email Sent Icon"
+                        className="icon"
+                      />
+                      <a href="#">Pricing page visited</a>
                     </div>
                   </div>
+
                   <ul>
                     <li>
                       <div className="li-container">
-                      <div className='inner-li-container'>
-                        <img
-                          src={emailSentIcon}
-                          alt="Email Sent Icon"
-                          className="icon"
-                        />
-                        <a href="#">Send Connection Request</a>
+                        <div className="inner-li-container">
+                          <img
+                            src={emailSentIcon}
+                            alt="Email Sent Icon"
+                            className="icon"
+                          />
+                          <a href="#">Send Connection Request</a>
                         </div>
                       </div>
                       <ul>
                         <li>
                           <div className="li-container">
                             <div className="request-accepted">
-                              Connection Request Accepted
+                            <img
+                            src={emailSentIcon}
+                            alt="Email Sent Icon"
+                            className="icon"
+                          />
+                            <a href="#">Connection Request Accepted</a>
                             </div>
-                            <div className="li-parent">
+                            <div className="linkedIn-parent">
                               <div className="li-child">
                                 <img
                                   src={emailSentIcon}
@@ -85,65 +91,67 @@ export const Flowchart = () => {
                 </li>
               </ul>
             </li>
+
             <li>
               <div className="li-container">
-              <div className='inner-li-container'>
-                <img
-                  src={emailSentIcon}
-                  alt="Email Sent Icon"
-                  className="icon"
-                />
-                <div className="text-container">
-                  <a href="#">Email not opened</a>
-                </div>
+                <div className="inner-li-container">
+                  <img
+                    src={emailSentIcon}
+                    alt="Email Sent Icon"
+                    className="icon"
+                  />
+                  <div className="text-container">
+                    <a href="#">Email not opened</a>
+                  </div>
                 </div>
               </div>
+
               <ul>
                 <li>
-                  <div className="li-container">
-                  <div className='inner-li-container'>
-                    <img
-                      src={emailSentIcon}
-                      alt="Email Sent Icon"
-                      className="icon"
-                    />
-                    <a href="#">Send followup email</a>
+                  <div className="li-container first-child-extended"> {/* For extending line */}
+                    <div className="inner-li-container">
+                      <img
+                        src={emailSentIcon}
+                        alt="Email Sent Icon"
+                        className="icon"
+                      />
+                      <a href="#">Send followup email</a>
                     </div>
                   </div>
                   <ul>
                     <li>
                       <div className="li-container zoom-effect">
-                      <div className='inner-li-container'>
-                        <img
-                          src={emailSentIcon}
-                          alt="Email Sent Icon"
-                          className="icon"
-                        />
-                        <a href="#">Visit LinkedIn Profile</a>
+                        <div className="inner-li-container">
+                          <img
+                            src={emailSentIcon}
+                            alt="Email Sent Icon"
+                            className="icon"
+                          />
+                          <a href="#">Visit LinkedIn Profile</a>
                         </div>
                       </div>
                       <ul>
                         <li>
                           <div className="li-container zoom-effect">
-                          <div className='inner-li-container'>
-                            <img
-                              src={emailSentIcon}
-                              alt="Email Sent Icon"
-                              className="icon"
-                            />
-                            <a href="#">Send LinkedIn Inmail</a>
+                            <div className="inner-li-container">
+                              <img
+                                src={emailSentIcon}
+                                alt="Email Sent Icon"
+                                className="icon"
+                              />
+                              <a href="#">Send LinkedIn Inmail</a>
                             </div>
                           </div>
                           <ul>
                             <li>
                               <div className="li-container">
-                              <div className='inner-li-container'>
-                                <img
-                                  src={emailSentIcon}
-                                  alt="Email Sent Icon"
-                                  className="icon"
-                                />
-                                <a href="#">Send connection Request</a>
+                                <div className="inner-li-container">
+                                  <img
+                                    src={emailSentIcon}
+                                    alt="Email Sent Icon"
+                                    className="icon"
+                                  />
+                                  <a href="#">Send connection Request</a>
                                 </div>
                               </div>
                             </li>
