a58253ba8fc0b8da66de89dc69df2025ece2ea7d
hrithik+1-at-970287672632 <hrithik@salesgear.io>
2024-10-09 07:57:56 +0530

    Note this commit. In case if we loose the code. Till this it is okay

---
+++ Marketing website 2/mb-2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Flowchart } from './component/Flowchart';
+
+function App() {
+  return (
+    <div className="App">
+      <Flowchart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Marketing website 2/mb-2/src/component/Flowchart.js
@@ -0,0 +1,163 @@
+import emailSentIcon from "../Assets/download.svg";
+
+export const Flowchart = () => {
+  return (
+    <div className="tree">
+      <ul>
+        <li>
+          <div className="li-container mailSent">
+          <div className='mailSent-div'>
+            <img src={emailSentIcon} alt="Email Sent Icon" className="icon" />
+            <a className="mailSentAnchor" href="#">
+              Email Sent
+            </a>
+            </div>
+
+          </div>
+
+          <ul>
+            <li>
+              <div className="li-container">
+                <div className='inner-li-container'>
+                  <img
+                    src={emailSentIcon}
+                    alt="Email Sent Icon"
+                    className="icon"
+                  />
+                  <div className="text-container">
+                    <a href="#"> Email Opened twice</a>
+                  </div>
+                </div>
+              </div>
+              <ul>
+                <li>
+                  <div className="li-container">
+                  <div className='inner-li-container'>
+                    <img
+                      src={emailSentIcon}
+                      alt="Email Sent Icon"
+                      className="icon"
+                    />
+                    <a href="#">Pricing page visited</a>
+                    </div>
+                  </div>
+                  <ul>
+                    <li>
+                      <div className="li-container">
+                      <div className='inner-li-container'>
+                        <img
+                          src={emailSentIcon}
+                          alt="Email Sent Icon"
+                          className="icon"
+                        />
+                        <a href="#">Send Connection Request</a>
+                        </div>
+                      </div>
+                      <ul>
+                        <li>
+                          <div className="li-container">
+                            <div className="request-accepted">
+                              Connection Request Accepted
+                            </div>
+                            <div className="li-parent">
+                              <div className="li-child">
+                                <img
+                                  src={emailSentIcon}
+                                  alt="Email Sent Icon"
+                                  className="icon"
+                                />
+                                <a href="#">Send LinkedIn Message</a>
+                              </div>
+                              <div className="li-child">
+                                <img
+                                  src={emailSentIcon}
+                                  alt="Email Sent Icon"
+                                  className="icon"
+                                />
+                                <a href="#">Call the Prospect</a>
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <div className="li-container">
+              <div className='inner-li-container'>
+                <img
+                  src={emailSentIcon}
+                  alt="Email Sent Icon"
+                  className="icon"
+                />
+                <div className="text-container">
+                  <a href="#">Email not opened</a>
+                </div>
+                </div>
+              </div>
+              <ul>
+                <li>
+                  <div className="li-container">
+                  <div className='inner-li-container'>
+                    <img
+                      src={emailSentIcon}
+                      alt="Email Sent Icon"
+                      className="icon"
+                    />
+                    <a href="#">Send followup email</a>
+                    </div>
+                  </div>
+                  <ul>
+                    <li>
+                      <div className="li-container zoom-effect">
+                      <div className='inner-li-container'>
+                        <img
+                          src={emailSentIcon}
+                          alt="Email Sent Icon"
+                          className="icon"
+                        />
+                        <a href="#">Visit LinkedIn Profile</a>
+                        </div>
+                      </div>
+                      <ul>
+                        <li>
+                          <div className="li-container zoom-effect">
+                          <div className='inner-li-container'>
+                            <img
+                              src={emailSentIcon}
+                              alt="Email Sent Icon"
+                              className="icon"
+                            />
+                            <a href="#">Send LinkedIn Inmail</a>
+                            </div>
+                          </div>
+                          <ul>
+                            <li>
+                              <div className="li-container">
+                              <div className='inner-li-container'>
+                                <img
+                                  src={emailSentIcon}
+                                  alt="Email Sent Icon"
+                                  className="icon"
+                                />
+                                <a href="#">Send connection Request</a>
+                                </div>
+                              </div>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+};
