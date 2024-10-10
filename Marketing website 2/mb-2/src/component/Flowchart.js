219df92380e8a1b9c1ef67d4759e54dd8df18afa
hrithik+1-at-970287672632 <hrithik@salesgear.io>
2024-10-10 15:49:04 +0530

    Note this commit. Reduced icon sizeinside the connection request accepted

--- Marketing website 2/mb-2/src/component/Flowchart.js
+++ Marketing website 2/mb-2/src/component/Flowchart.js
@@ -19,7 +19,10 @@ export const Flowchart = () => {
     function applyBlur() {
       console.log("Blur applied");
       allContainers.forEach((li) => {
-        if (!li.classList.contains("zoom-effect")) {
+        if (
+          !li.classList.contains("zoom-effect") &&
+          !li.classList.contains("zoom-avoid")
+        ) {
           li.classList.add("blur-applied");
         }
       });
@@ -50,203 +53,203 @@ export const Flowchart = () => {
 
   return (
     <>
-    <div className="background-image"></div>
-    <div className="tree">
-      <ul>
-        <li>
-          <div className="li-container mailSent">
-            <div className="mailSent-div">
-              <div className="mailsent-icon">
-                {" "}
-                <img
-                  src={emailSentIcon}
-                  alt="Email Sent Icon"
-                  className="email-icon"
-                />
-              </div>
-              <a className="mailSentAnchor" href="#">
-                Email Sent
-              </a>
-            </div>
-          </div>
-
-          <ul>
-            <li>
-              <div className="li-container parent-container">
-                <div className="inner-li-container">
-                  <div className="emailOpenedTwice-icon">
-                    <img
-                      src={emailOpenedTwice}
-                      alt="Email opened twice Icon"
-                      className="icon2"
-                    />
-                  </div>
-                  <div className="text-container">
-                    <a href="#"> Email Opened twice</a>
-                  </div>
+      <div className="background-image"></div>
+      <div className="tree">
+        <ul>
+          <li>
+            <div className="li-container mailSent">
+              <div className="mailSent-div">
+                <div className="mailsent-icon">
+                  {" "}
+                  <img
+                    src={emailSentIcon}
+                    alt="Email Sent Icon"
+                    className="email-icon"
+                  />
                 </div>
+                <a className="mailSentAnchor" href="#">
+                  Email Sent
+                </a>
               </div>
+            </div>
 
-              <ul>
-                <li>
-                  <div className="li-container">
-                    <div className="inner-li-container-child-icon">
-                      <div className="icon-bg">
-                        <img
-                          src={pricingPageVisited}
-                          alt="Email Sent Icon"
-                          className="icon"
-                        />
-                      </div>
-                      <a href="#">Pricing page visited</a>
+            <ul>
+              <li>
+                <div className="li-container parent-container">
+                  <div className="inner-li-container">
+                    <div className="emailOpenedTwice-icon">
+                      <img
+                        src={emailOpenedTwice}
+                        alt="Email opened twice Icon"
+                        className="icon2"
+                      />
+                    </div>
+                    <div className="text-container">
+                      <a href="#"> Email Opened twice</a>
                     </div>
                   </div>
+                </div>
 
-                  <ul>
-                    <li>
-                      <div className="li-container">
-                        <div className="inner-li-container-child-icon">
-                          <div className="icon-bg">
-                            <img
-                              src={sendConnectionRequest}
-                              alt="Email Sent Icon"
-                              className="icon"
-                            />
-                          </div>
-                          <a href="#">Send Connection Request</a>
+                <ul>
+                  <li>
+                    <div className="li-container">
+                      <div className="inner-li-container-child-icon">
+                        <div className="icon-bg">
+                          <img
+                            src={pricingPageVisited}
+                            alt="Email Sent Icon"
+                            className="icon"
+                          />
                         </div>
+                        <a href="#">Pricing page visited</a>
                       </div>
-                      <ul>
-                        <li>
-                          <div className="li-container">
-                            <div className="request-accepted">
+                    </div>
+
+                    <ul>
+                      <li>
+                        <div className="li-container">
+                          <div className="inner-li-container-child-icon">
+                            <div className="icon-bg">
                               <img
-                                src={connectionRequestAccepted}
+                                src={sendConnectionRequest}
                                 alt="Email Sent Icon"
                                 className="icon"
                               />
-                              <a href="#">Connection Request Accepted</a>
                             </div>
-                            <div className="linkedIn-parent">
-                              <div className="li-child">
-                              {/* <div className="li-connection-icon-bg"> */}
+                            <a href="#">Send Connection Request</a>
+                          </div>
+                        </div>
+                        <ul>
+                          <li>
+                            <div className="li-container zoom-avoid">
+                              <div className="request-accepted">
                                 <img
-                                  src={sendLinkedinMessage}
-                                  alt="Email Sent Icon"
+                                  src={connectionRequestAccepted}
+                                  alt="Request Accepted Icon"
                                   className="icon"
                                 />
-                                {/* </div> */}
-                                <a href="#">Send LinkedIn Message</a>
+                                <a href="#">Connection Request Accepted</a>
                               </div>
-                              <div className="li-child">
-                                <div className="prospect-icon">
+                              <div className="linkedIn-parent">
+                                <div className="li-child">
+                                  <div className="connection-accepted-icon-bg">
+                                    <img
+                                      src={sendLinkedinMessage}
+                                      alt="Message Sent Icon"
+                                      className="connection-accepted-icon"
+                                    />
+                                  </div>
+                                  <a href="#" className="a-connection-request-accepted">Send LinkedIn Message</a>
+                                </div>
+                                <div className="li-child">
+                                  <div className="call-prospect-icon-bg zoom-effect">
+                                    <img
+                                      src={callTheProspect}
+                                      alt="call Icon"
+                                      className="call-prospect-icon"
+                                    />
+                                  </div>
+
+                                  <a href="#" className="call-prospect zoom-effect">
+                                    Call the Prospect
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+
+              <li>
+                <div className="li-container parent-container">
+                  <div className="inner-li-container">
+                    <div className="emailNotOpened-icon">
+                      <img
+                        src={emailNotOpened}
+                        alt="Email not opened Icon"
+                        className="icon3"
+                      />
+                    </div>
+                    <div className="text-container">
+                      <a href="#">Email not opened</a>
+                    </div>
+                  </div>
+                </div>
+
+                <ul>
+                  <li>
+                    <div className="li-container first-child-extended">
+                      <div className="inner-li-container-child-icon">
+                        <div className="icon-bg">
+                          <img
+                            src={sendFollowUpMail}
+                            alt="Email Sent Icon"
+                            className="icon"
+                          />
+                        </div>
+                        <a href="#">Send followup email</a>
+                      </div>
+                    </div>
+                    <ul>
+                      <li>
+                        <div className="li-container zoom-effect">
+                          <div className="inner-li-container-child-icon">
+                            <div className="icon-bg">
+                              <img
+                                src={visitLinkedinProfile}
+                                alt="Email Sent Icon"
+                                className="icon"
+                              />
+                            </div>
+                            <a href="#">Visit LinkedIn Profile</a>
+                          </div>
+                        </div>
+                        <ul>
+                          <li>
+                            <div className="li-container zoom-effect">
+                              <div className="inner-li-container-child-icon">
+                                <div className="icon-bg">
                                   <img
-                                    src={callTheProspect}
+                                    src={sendLinkedInInmail}
                                     alt="Email Sent Icon"
                                     className="icon"
                                   />
                                 </div>
-
-                                <a href="#" className="call-prospect">
-                                  Call the Prospect
-                                </a>
+                                <a href="#">Send LinkedIn Inmail</a>
                               </div>
                             </div>
-                          </div>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
+                            <ul>
+                              <li>
+                                <div className="li-container">
+                                  <div className="inner-li-container-child-icon">
+                                    <div className="icon-bg">
+                                      <img
+                                        src={sendConnectionRequest}
+                                        alt="Email Sent Icon"
+                                        className="icon"
+                                      />
+                                    </div>
 
-            <li>
-              <div className="li-container parent-container">
-                <div className="inner-li-container">
-                  <div className="emailNotOpened-icon">
-                    <img
-                      src={emailNotOpened}
-                      alt="Email not opened Icon"
-                      className="icon3"
-                    />
-                  </div>
-                  <div className="text-container">
-                    <a href="#">Email not opened</a>
-                  </div>
-                </div>
-              </div>
-
-              <ul>
-                <li>
-                  <div className="li-container first-child-extended">
-                    <div className="inner-li-container-child-icon">
-                      <div className="icon-bg">
-                        <img
-                          src={sendFollowUpMail}
-                          alt="Email Sent Icon"
-                          className="icon"
-                        />
-                      </div>
-                      <a href="#">Send followup email</a>
-                    </div>
-                  </div>
-                  <ul>
-                    <li>
-                      <div className="li-container zoom-effect">
-                        <div className="inner-li-container-child-icon">
-                          <div className="icon-bg">
-                            <img
-                              src={visitLinkedinProfile}
-                              alt="Email Sent Icon"
-                              className="icon"
-                            />
-                          </div>
-                          <a href="#">Visit LinkedIn Profile</a>
-                        </div>
-                      </div>
-                      <ul>
-                        <li>
-                          <div className="li-container zoom-effect">
-                            <div className="inner-li-container-child-icon">
-                              <div className="icon-bg">
-                                <img
-                                  src={sendLinkedInInmail}
-                                  alt="Email Sent Icon"
-                                  className="icon"
-                                />
-                              </div>
-                              <a href="#">Send LinkedIn Inmail</a>
-                            </div>
-                          </div>
-                          <ul>
-                            <li>
-                              <div className="li-container">
-                                <div className="inner-li-container-child-icon">
-                                  <div className="icon-bg">
-                                    <img
-                                      src={sendConnectionRequest}
-                                      alt="Email Sent Icon"
-                                      className="icon"
-                                    />
+                                    <a href="#">Send connection Request</a>
                                   </div>
-
-                                  <a href="#">Send connection Request</a>
                                 </div>
-                              </div>
-                            </li>
-                          </ul>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
